1ed65eb0f155dda6190df7fd67fe348d558bee5b
Anton Kostin <schnibba@gmail.com>
2016-03-20 17:18:28 +0300

    Add default host options to production env 2.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net' }
+  config.action_mailer.default_url_options = { :host => 'eve-trader.net' }
 
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV["SENDGRID_USER"],
